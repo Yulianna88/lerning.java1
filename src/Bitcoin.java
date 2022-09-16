a1d3c65575c584ae8fe5d9b9de85e58976712dc6
Yulianna88 <113635615+Yulianna88@users.noreply.github.com>
2022-09-16 23:03:38 +0200

    Initial commit

---
+++ src/Bitcoin.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bitcoin {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Do you want to now prise Bitcoin? Enter $ amount:");
+        int x = s.nextInt();
+        System.out.println(x+ "/19,720");
+    }
+}
